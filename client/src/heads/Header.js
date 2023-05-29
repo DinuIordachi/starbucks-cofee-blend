7894756a0a717ef5f7b11c1e25de0a694569ba8b
Dumitru Iordachi <dumitru.iordachi@mindthevalue.com>
2023-05-29 22:18:20 +0300

    add bootstrap

---
+++ client/src/heads/Header.js
@@ -0,0 +1,25 @@
+import {Container, Navbar} from "react-bootstrap";
+import logo from "../assets/logo.png"
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            React Bootstrap
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default Header
